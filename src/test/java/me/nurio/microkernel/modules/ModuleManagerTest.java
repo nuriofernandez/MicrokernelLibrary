df41dec1966a3b658ff957cf3d57e5a8172a5cc0
Nurio Fernández <personal@nurio.me>
2020-10-08 18:48:24 +0200

    Added ModuleManagerTest

---
+++ src/test/java/me/nurio/microkernel/modules/ModuleManagerTest.java
@@ -0,0 +1,56 @@
+package me.nurio.microkernel.modules;
+
+import lombok.Getter;
+import me.nurio.events.EventManager;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ModuleManagerTest {
+
+    private ModuleTest module;
+    private EventManager eventManager;
+    private ModuleManager moduleManager;
+
+    @Before
+    public void start() {
+        module = new ModuleTest();
+        eventManager = new EventManager();
+        moduleManager = new ModuleManager(eventManager);
+    }
+
+    @Test
+    public void testLoadModule() {
+        moduleManager.loadModule(module);
+        assertTrue(module.isEnabled());
+    }
+
+    @Test
+    public void testUnloadModule() {
+        moduleManager.unloadModule(module);
+        assertTrue(module.isDisabled());
+    }
+
+}
+
+@Getter
+class ModuleTest implements Module {
+
+    private String name = "Test";
+    private String author = "Nurio";
+
+    private boolean enabled;
+    private boolean disabled;
+
+    @Override
+    public void onEnable() {
+        enabled = true;
+    }
+
+    @Override
+    public void onDisable() {
+        disabled = true;
+    }
+
+}
